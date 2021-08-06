672b27b60fbc7be669fae09b61cb2918fbaeeb11
mintavi <kmintavi@gmail.com>
2021-08-06 17:14:59 +0900

    testing

--- lib/flutter_naver_login_web.dart
+++ lib/flutter_naver_login_web.dart
@@ -5,16 +5,25 @@ import 'web/inject_js_libraries.dart';
 import 'web/naver_login.dart';
 
 class FlutterNaverLoginPlugin {
-  final String _clientId = "5olG0LvBwCIDpaaFXOZH";
-  final String _callbackUrl = "www.google.com";
+  static late final String? _clientId;
+  static late final String? _callbackUrl;
 
   static initialize() async {
+    print("in initialize");
     const String naverApiUrl = 'https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js';
     const String jQueryUrl = 'http://code.jquery.com/jquery-1.11.3.min.js';
     await injectJSLibraries([naverApiUrl, jQueryUrl]);
+
+    _clientId = html.window.document.querySelector("meta[name='naver-login-client-id']")
+                    ?.getAttribute("content");
+    _callbackUrl = html.window.document.querySelector("meta[name='naver-login-callback-url']")
+                    ?.getAttribute("content");
+    print("clientId: $_clientId");
+    print("callbackUrl: $_callbackUrl");
   }
 
   static void registerWith(Registrar registrar) async {
+    print("in registerWith");
     await initialize();
     final MethodChannel channel = MethodChannel(
       "flutter_naver_login",
@@ -45,13 +54,23 @@ class FlutterNaverLoginPlugin {
   }
 
   _logIn() {
+    print("in _logIn");
     // init
-    var naverIdLogin = new naver_id_login(_clientId, _callbackUrl);
+    if (_clientId == null) {
+      html.window.alert("네이버 아이디 로그인 Client ID가 필요합니다.");
+      return;
+    }
+    if (_callbackUrl == null) {
+      html.window.alert("네이버 아이디 로그인 callback Url이 필요합니다.");
+      return;
+    }
+
+    var naverIdLogin = new naver_id_login(_clientId!, _callbackUrl!);
     var state = naverIdLogin.getUniqState();
     naverIdLogin.setDomain(".fanmeet-dev.rftap.net");
     naverIdLogin.setState(state);
     naverIdLogin.setPopup(true);
-    // naverIdLogin.init_naver_id_login();
+    naverIdLogin.init_naver_id_login();
   }
 
   _logOut() {
