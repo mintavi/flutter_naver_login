2eefdd4544f30f8de2b8dbbf9f8be83ad6e3e8fd
mintavi <kmintavi@gmail.com>
2021-08-08 23:52:39 +0900

    make jsCallback funcion global

--- assets/process_callback.js
+++ assets/process_callback.js
@@ -6,18 +6,18 @@ var process_callback = function() {
                         
     var nil = new naver_id_login(_clientId, _callbackUrl);
 
-    nil.init_naver_id_login_callback();  print("after init_naver_login_callback");
+    nil.init_naver_id_login_callback();  console.log("after init_naver_login_callback");
     nil.get_naver_userprofile("jsCallback()");
+}
 
-    var jsCallback = function() {
-        console.log(nil.getProfileData('age'));
+var jsCallback = function() {
+    console.log(nil.getProfileData('age'));
 
-        var opener = window.opener;
-        if (opener == null) {
-            window.open("http://localhost:10000/#/login", "opener");
-        }
-        // opener.postMessage(result, "http://localhost:10000/#/login");
-        // opener.postMessage(result, "*");
-        window.close();
+    var opener = window.opener;
+    if (opener == null) {
+        window.open("http://localhost:10000/#/login", "opener");
     }
+    // opener.postMessage(result, "http://localhost:10000/#/login");
+    // opener.postMessage(result, "*");
+    window.close();
 }
